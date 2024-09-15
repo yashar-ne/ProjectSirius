b303c8a34cc7e0b83064da5d47745478dc3959ba
Yashar Nehls <yashar.ne@pm.me>
2024-09-15 09:35:11 +0200

    cleanup

--- src/Sirius/Sirius.API/Program.cs
+++ src/Sirius/Sirius.API/Program.cs
@@ -1,11 +1,4 @@
-using Common.Data;
-using System;
-using System.Configuration;
-using System.Reflection;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Sirius.API.Extensions;
-using Sirius.Scraping.Application.Forms.Queries;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,30 +22,4 @@ app.RegisterEndpointDefinitions();
 
 app.UseHttpsRedirection();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-//
-// app.MapGet("/weatherforecast", () =>
-//     {
-//         var forecast = Enumerable.Range(1, 5).Select(index =>
-//                 new WeatherForecast
-//                 (
-//                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//                     Random.Shared.Next(-20, 55),
-//                     summaries[Random.Shared.Next(summaries.Length)]
-//                 ))
-//             .ToArray();
-//         return forecast;
-//     })
-//     .WithName("GetWeatherForecast")
-//     .WithOpenApi();
-
-
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
